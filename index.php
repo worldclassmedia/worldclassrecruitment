2312007a7f569a714fd84e66013ba79f0df7f676
jasonkingworldclass <WorldClassMedia@Worlds-Mac-mini-1648.local>
2014-07-21 13:57:31 +0100

    New file.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php echo "Hello World and mars no, that's not rifght.";?>
+</body>
+</html>
